382e8beb0569369dabe2aeb08bac6d604936d774
erinmcmahon26 <70080908+erinmcmahon26@users.noreply.github.com>
2022-04-16 14:24:15 -0600

    annoying

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-import dash_bootstrap_components as dbc
+import sys
+print(sys.executable)
+
+#import dash_bootstrap_components as dbc
 import dash
 from dash import Input, Output, State, html, dcc
 import plotly.graph_objects as go
 import plotly.express as px
 
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.COSMO])
+app = dash.Dash()
 df = px.data.stocks()
 server = app.server
-app.config.suppress_callback_exceptions = True
+#app.config.suppress_callback_exceptions = True
 
 app.layout = html.Div(id='parent', children=[
     html.H1(id='H1', children='Styling using html components', style={'textAlign': 'center', \
@@ -17,3 +20,6 @@ app.layout = html.Div(id='parent', children=[
     #dcc.Graph(id='line_plot', figure=stock_prices())
 ]
                       )
+
+if __name__ == "__main__":
+    app.run_server(debug=True, host="0.0.0.0", port=8080, use_reloader=False)
