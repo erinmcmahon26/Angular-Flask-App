c97582039b4232a77fc7b9c348410b67ed83ae16
erinmcmahon26 <70080908+erinmcmahon26@users.noreply.github.com>
2022-04-16 12:56:05 -0600

    adding more files

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-import webapp2
+import dash_bootstrap_components as dbc
+import dash
+from dash import Input, Output, State, html, dcc
+import plotly.graph_objects as go
+import plotly.express as px
 
-class HelloWorld(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello from App Engine!');
 
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.COSMO])
+df = px.data.stocks()
+server = app.server
+app.config.suppress_callback_exceptions = True
 
-app = webapp2.WSGIApplication([('/', HelloWorld), ])
+app.layout = html.Div(id='parent', children=[
+    html.H1(id='H1', children='Styling using html components', style={'textAlign': 'center', \
+                                                                      'marginTop': 40, 'marginBottom': 40}),
+
+    #dcc.Graph(id='line_plot', figure=stock_prices())
+]
+                      )
