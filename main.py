25fc2ae7637dfa76f2aa29f081756a0f4a4f1e50
erinmcmahon26 <70080908+erinmcmahon26@users.noreply.github.com>
2022-04-17 15:07:05 -0600

    change port

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask_restful import Api
 
 app = Flask(__name__)
+api = Api(app)
 
 @app.route('/')
 def index():
     return '{"hello" : "otis"}'
 
-app.run(host='0.0.0.0', port = 8080)
