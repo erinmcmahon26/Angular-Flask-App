175d30fb0664c785c3501b616134ec1d94c725a9
erinmcmahon26 <70080908+erinmcmahon26@users.noreply.github.com>
2022-04-24 11:24:16 -0600

    added bigquery code to main.py file for flask app

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from flask import Flask
 from flask_restful import Api
+from google.cloud import bigquery
 
 app = Flask(__name__)
 api = Api(app)
 
+# constructing a BQ client object
+client = bigquery.Client()
+
+query = """
+    SELECT *
+    FROM bigquery-public-data.noaa_tsunami.historical_source_event
+    LIMIT 20
+"""
+
+query_job = client.query(query) # make an API request
+
+df = query_job.to_dataframe()
+json_object = df.to_json(orient='records')
+
 @app.route('/')
-def index():
-    return '{"hello" : "otis"}'
+def query():
+    response = json_object
+    return response
 
