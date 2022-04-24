ac63d73197e251ad65db0a3934e3d18d5cce9728
erinmcmahon26 <70080908+erinmcmahon26@users.noreply.github.com>
2022-04-24 11:17:32 -0600

    changing dataset

--- bigquery.py
+++ bigquery.py
@@ -4,11 +4,8 @@ from google.cloud import bigquery
 client = bigquery.Client()
 
 query = """
-    SELECT name, SUM(number) as total_people
-    FROM `bigquery-public-data.usa_names.usa_1910_2013`
-    WHERE state = 'TX'
-    GROUP BY name, state
-    ORDER BY total_people DESC
+    SELECT *
+    FROM bigquery-public-data.noaa_tsunami.historical_source_event
     LIMIT 20
 """
 
