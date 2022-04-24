9ee6578d7cc00f4a5d8af27e6b1f909de9c5e0c5
erinmcmahon26 <70080908+erinmcmahon26@users.noreply.github.com>
2022-04-24 11:02:22 -0600

    trying to fix cloud error

--- bigquery.py
+++ bigquery.py
@@ -4,9 +4,13 @@ from google.cloud import bigquery
 client = bigquery.Client()
 
 query = """
-    SELECT * 
-    FROM 'bigquery-public-data.noaa_tsunami.historical_source_event'
-    LIMIT 100"""
+    SELECT name, SUM(number) as total_people
+    FROM `bigquery-public-data.usa_names.usa_1910_2013`
+    WHERE state = 'TX'
+    GROUP BY name, state
+    ORDER BY total_people DESC
+    LIMIT 20
+"""
 
 query_job = client.query(query) # make an API request
 
