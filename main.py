142ab33db8b08583bc3eb6488bb06fc16302ec8f
erinmcmahon26 <70080908+erinmcmahon26@users.noreply.github.com>
2022-05-22 15:23:14 -0600

    changed Flask app to Dash app

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from flask import Flask
-from flask_restful import Api
+# from flask import Flask
+# from flask_restful import Api
+from dash import Dash, dcc, html, Input, Output
 from google.cloud import bigquery
 import plotly
 import plotly.express as px
 
-app = Flask(__name__)
-api = Api(app)
-
+# app = Flask(__name__)
+# api = Api(app)
+app = Dash(__name__)
 # constructing a BQ client object
 client = bigquery.Client()
 
@@ -20,11 +21,18 @@ query_job = client.query(query) # make an API request
 
 df = query_job.to_dataframe()
 #json_object = df.to_json(orient='records')
-fig = px.line(df, x='time_series_timestamp', y ='time_series_data', title = 'World Population')
-fig.show()
+fig = px.line(df, x='time_series_timestamp', y ='time_series_data')
 
-@app.route('/', methods=['GET'])
-def query():
-    response = fig
-    return response
+
+app.layout = html.Div(children = [
+    html.H1("World Population Forcast"),
+    html.Div(children ='''An app to see what the expected world population will be each year over the next 10 years.'''),
+    dcc.Graph(
+        id = 'World Population Graph',
+        figure = fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
 
